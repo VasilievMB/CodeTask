7cec01600e2b03581ec836108ed94972356538cc
Vasiljev M.B <vasiljevmb@sib-soft.ru>
2016-06-16 14:17:55 +0700

    added app icon, bundle id changed

--- CodeTask/CodeTask/Modules/SignIn/View/CDTSignInViewController.m
+++ CodeTask/CodeTask/Modules/SignIn/View/CDTSignInViewController.m
@@ -28,7 +28,7 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-        
+    
     self.signInBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Sign In", @"")
                                                                 style:UIBarButtonItemStylePlain
                                                                target:self
